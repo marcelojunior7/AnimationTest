c3f5302d4e4ec4c7faf66ab59ae87d46f7de3395
Rodrigo Prestes <rodrigoprestes@me.com>
2013-08-01 16:36:05 -0300

    Adicionado arquivos do sidescroller

---
+++ AnimationTest/AnimationTest/SideScrollerViewController.h
@@ -0,0 +1,13 @@
+//
+//  SideScrollerViewController.h
+//  AnimationTest
+//
+//  Created by Rodrigo Prestes on 01/08/13.
+//  Copyright (c) 2013 RodPrestes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SideScrollerViewController : UIViewController
+
+@end
---
+++ AnimationTest/AnimationTest/SideScrollerViewController.m
@@ -0,0 +1,66 @@
+//
+//  SideScrollerViewController.m
+//  AnimationTest
+//
+//  Created by Rodrigo Prestes on 01/08/13.
+//  Copyright (c) 2013 RodPrestes. All rights reserved.
+//
+
+#import "SideScrollerViewController.h"
+
+@interface SideScrollerViewController ()
+
+@end
+
+@implementation SideScrollerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    
+    if(orientation == 0 || orientation == UIInterfaceOrientationPortrait || orientation == UIInterfaceOrientationPortraitUpsideDown)
+    {
+        [self showPortraitAlert];
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+#pragma orientation
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    if(toInterfaceOrientation == 0 || toInterfaceOrientation == UIInterfaceOrientationPortrait || toInterfaceOrientation == UIInterfaceOrientationPortraitUpsideDown)
+    {
+        [self showPortraitAlert];
+    }
+}
+
+
+#pragma render
+
+- (void)showPortraitAlert
+{
+    [[[self.view subviews] objectAtIndex:[[self.view subviews] count] - 1] removeFromSuperview];
+    
+    UIView *viewContainer = [[UIView alloc] init];
+    [viewContainer setBackgroundColor:[UIColor lightGrayColor]];
+}
+
+
+@end
